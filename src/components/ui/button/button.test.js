338db4c2a5d1fee8912f7f7efdeb2a144f02e475
George051191 <trubacheff_91@mail.ru>
2022-06-24 11:18:23 +0300

    test button

---
+++ src/components/ui/button/button.test.js
@@ -0,0 +1,38 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import renderer from 'react-test-renderer'
+import React from "react";
+import { Button } from "./button";
+
+describe('Кнопка рендерится без ошибок', () => {
+    it('Рендер кнопки с текстом', () => {
+        const tree = renderer.create(< Button text='test text' />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('Рендер кнопки без текста', () => {
+        const tree = renderer.create(< Button />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('Рендер заблокированной кнопки', () => {
+        const tree = renderer.create(< Button disabled={true} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('Рендер кнопки с индикацией загрузки', () => {
+        const tree = renderer.create(< Button isLoader={true} />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    
+
+});
+
+it('Нажатие на кнопку вызывает колбек', () => {
+    window.alert = jest.fn();
+    render(<Button text='кнопка' onClick={()=>alert('Работает!')} />);
+    const button = screen.getByText('кнопка');
+    fireEvent.click(button);
+    expect(window.alert).toHaveBeenCalledWith('Работает!')
+});
+
---
+++ src/components/ui/circle/circle.test.js
@@ -0,0 +1,11 @@
+/* без буквы;
+с буквами (Snapshot);
+с head (Snapshot);
+с react-элементом в head;
+с tail;
+с react-элементом в tail;
+с index;
+с пропом isSmall ===  true;
+в состоянии default;
+в состоянии changing;
+в состоянии modified. */
