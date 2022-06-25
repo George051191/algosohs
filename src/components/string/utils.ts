f4468d2c52f20d0e164ce089b01bb03572f6cf7b
George051191 <trubacheff_91@mail.ru>
2022-06-25 08:25:04 +0300

    tests for string created

--- src/components/string/utils.ts
+++ src/components/string/utils.ts
@@ -18,3 +18,39 @@ export const findIndex = (
     return ElementStates.Modified;
   }
 };
+
+
+export const reverseStarting = async (value: string,
+  circleStateCallback: React.Dispatch<React.SetStateAction<boolean>>,
+  inputStateCallback: React.Dispatch<React.SetStateAction<boolean>>,
+  buttonStateCallback: React.Dispatch<React.SetStateAction<boolean>>,
+  helper: (milliseconds: number) => Promise<unknown>,
+  valueStateCallback: React.Dispatch<React.SetStateAction<string>>,
+  startStateCallback: React.Dispatch<React.SetStateAction<number | undefined>>,
+  endStateCallback: React.Dispatch<React.SetStateAction<number | undefined>>
+): Promise<void> => {
+  circleStateCallback(true);
+  inputStateCallback(true);
+  buttonStateCallback(true);
+  let arr = value.split("");
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    await helper(1000);
+    startStateCallback(start);
+    endStateCallback(end);
+    await helper(1000)
+    const temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    valueStateCallback(arr.join(""));
+    await helper(1000)
+    start++;
+    end--;
+  }
+  startStateCallback(13);
+  endStateCallback(13);
+  inputStateCallback(false);
+  buttonStateCallback(false);
+
+}
